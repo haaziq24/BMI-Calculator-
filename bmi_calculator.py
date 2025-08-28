f94deae81fa58cdbfe203bd7f5ca24a6e2430847
Haaziq Abbasi <haaziq24@gmail.com>
2025-08-28 01:59:49 -0400

    Add files via upload

---
+++ bmi_calculator.py
@@ -0,0 +1,6 @@
+mass = float(input('Enter your weight(kg): '))
+height = float(input('Enter your height(meters): '))
+
+bmi = mass/height **2 
+
+print(f"Your BMI based off your meaurements is {bmi:.1f}")
